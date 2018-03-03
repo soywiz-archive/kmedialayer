4f1a9b5ae6be1942ded29eed0bb1e4c7182eeaab
soywiz <soywiz@gmail.com>
2018-03-03 17:17:05 +0100

    Webgl mouse and keyboard events

--- kmedialayer/js/src/main/kotlin/com/soywiz/kmedialayer/KmlJs.kt
+++ kmedialayer/js/src/main/kotlin/com/soywiz/kmedialayer/KmlJs.kt
@@ -1,6 +1,7 @@
 package com.soywiz.kmedialayer
 
 import org.w3c.dom.*
+import org.w3c.dom.events.*
 import kotlin.browser.*
 
 actual object Kml {
@@ -10,8 +11,33 @@ actual object Kml {
             id = "kml-canvas"
             width = windowConfig.width
             height = windowConfig.height
-            document.appendChild(this)
+            document.body!!.appendChild(this)
         })) as HTMLCanvasElement
+
+        var mouseX = 0
+        var mouseY = 0
+        var mouseButtons = 0
+
+        fun mouseUpdate(me: MouseEvent) {
+            val pos = canvas.getBoundingClientRect()
+            mouseX = me.clientX - pos.left.toInt()
+            mouseY = me.clientY - pos.top.toInt()
+            mouseButtons = me.buttons.toInt()
+            listener.mouseUpdate(mouseX, mouseY, mouseButtons)
+        }
+
+        window.addEventListener("mousemove", { e: Event -> mouseUpdate(e as MouseEvent) })
+        window.addEventListener("mousedown", { e: Event -> mouseUpdate(e as MouseEvent) })
+        window.addEventListener("mouseup", { e: Event -> mouseUpdate(e as MouseEvent) })
+
+        window.addEventListener("keydown", { e: Event ->
+            listener.keyUpdate((e as KeyboardEvent).keyCode, true)
+        })
+
+        window.addEventListener("keyup", { e: Event ->
+            listener.keyUpdate((e as KeyboardEvent).keyCode, false)
+        })
+
         val gl = KmlGlJsCanvas(canvas)
 
         listener.init(gl)
