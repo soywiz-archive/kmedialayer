cb5a99dca0fa566335cefb2c5c48fb2ca448d679
soywiz <soywiz@gmail.com>
2018-03-07 22:28:22 +0100

    Some more work

--- sample_scene.kts
+++ sample_scene.kts
@@ -7,3 +7,11 @@
 @file:MavenRepository("soywiz-bintray","https://dl.bintray.com/soywiz/soywiz/" )
 
 import com.soywiz.kmedialayer.*
+import com.soywiz.kmedialayer.scene.*
+
+SceneApplication { object : Scene() {
+    override suspend fun init() {
+        root += Image(texture("mini.png")).apply {
+        }
+    }
+} }
